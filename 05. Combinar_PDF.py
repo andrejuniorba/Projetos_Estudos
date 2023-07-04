281a020a6216cae3be9f07ac1d6697199a92dd45
André Luz <68282848+andrejuniorba@users.noreply.github.com>
2023-07-04 18:42:52 -0300

    Update 05. Combinar_PDF.py

--- 05. Combinar_PDF.py
+++ 05. Combinar_PDF.py
@@ -1,9 +1,9 @@
 #pip install pypdf
-from pypdf import PdfWriter
+from pypdf import PdfMerger
 
-merger = PdfWriter()
+merger = PdfMerger()
 
-lista = ('arquivo1.pdf', 'arquivo2.pdf', 'arquivo3.pdf')
+lista = ['arquivo1.pdf', 'arquivo2.pdf', 'arquivo3.pdf']
 
 for pdf in lista:
     merger.append(pdf)
